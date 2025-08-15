ffe55c544a7f6d9d25ab3d08cd27bab2a4e5e244
Pleezon <pleezon@javadiscord.net>
2025-08-15 16:03:54 +0200

    vercel analytics

--- HutTrip/src/app/app.component.ts
+++ HutTrip/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ControlBarComponentComponent } from './components/control-bar-component/control-bar-component.component';
-import { HutListComponentComponent } from './components/hut-list-component/hut-list-component.component';
+import {ControlBarComponentComponent} from './components/control-bar-component/control-bar-component.component';
+import {HutListComponentComponent} from './components/hut-list-component/hut-list-component.component';
+import {inject} from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,12 @@ import { HutListComponentComponent } from './components/hut-list-component/hut-l
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Hut Trip Planner';
-  desc='Plan your perfect mountain adventure'
+  desc = 'Plan your perfect mountain adventure'
+
+  ngOnInit() {
+    inject();
+  }
 }
